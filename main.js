24ca95f8296916a2249b6a306fc88410a2f83166
MrYammys <85060421+MrYammys@users.noreply.github.com>
2022-03-22 18:58:33 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+
+function add() {
+	imgTag = new Image();
+	imgTag.onload = uploadimg;
+	imgTag.src = img_image;
+	console.log("function add is called");
+}
+function uploadimg() {
+
+	ctx.drawImage(imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+	console.log("key is pressed");
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+		aplhabetkey();
+		document.getElementById("d1").innerHTML = "You pressed an alphabet key";
+		console.log("alphabet key");
+	}
+	if ((keyPressed >= 48 && keyPressed <= 57)) {
+		numberkey();
+		document.getElementById("d1").innerHTML = "You pressed an number key";
+		console.log("number key");
+	}
+	if ((keyPressed >= 37 && keyPressed <= 40)) {
+		arrowkey();
+		document.getElementById("d1").innerHTML = "You pressed an arrow key";
+		console.log("arrow key");
+	}
+	if (keyPressed == 17) {
+		specialkey();
+		document.getElementById("d1").innerHTML = "You pressed an special key";
+		console.log("special key");
+	}
+	if (keyPressed == 18) {
+		specialkey();
+		document.getElementById("d1").innerHTML = "You pressed an special key";
+		console.log("special key");
+	}
+	if (keyPressed == 27) {
+		specialkey();
+		document.getElementById("d1").innerHTML = "You pressed an special key";
+		console.log("special key");
+	}
+
+}
+function aplhabetkey() {
+	img_image = "Alpkey.png";
+	add();
+}
+function numberkey() {
+	img_image = "numkey.png";
+	add();
+}
+function arrowkey() {
+	img_image = "Arrkey.png";
+	add();
+}
+function specialkey() {
+	img_image = "spkey.png";
+	add();
+}
+function otherkey() {
+	img_image = "otherkey.png";
+	add();
+}
